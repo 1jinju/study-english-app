40721908803f5e5537fce3c2091394952cb1bba5
1jinju <gkswlswn2044@gmail.com>
2023-04-27 01:25:58 +0900

    영어 데이터 API 결과 랜덤 출력

--- test1.js
+++ test1.js
@@ -6,7 +6,9 @@ const sentenceEl = document.querySelector('.sentence');
 
 let timeLimit = 3000; // 문제당 제한시간 3초
 let sentences = []; // API에서 가져온 전체 문장
+let totalSentences = 0; // 전체 문장 수
 let currentIndex = 0; // 현재 문장의 인덱스
+let currentSentence = ''; // 현재 문장
 
 const getSentences = async () => {
     fetch(listUrl)
@@ -22,10 +24,17 @@ const getSentences = async () => {
       });
 };
 
+// 랜덤으로 문장을 가져옴
+const getRandomSentence = (sentences) => {
+    const randomIndex = Math.floor(Math.random() * totalSentences);
+    return sentences[randomIndex];
+};
+
+// 문장 출력
 const showSentence = () => {
-    const sentence = sentences[currentIndex];
-    sentenceEl.querySelector('h2').textContent = sentence.ko;
-    sentenceEl.querySelector('h1').textContent = sentence.en; 
+    currentSentence = getRandomSentence(sentences);
+    sentenceEl.querySelector('h2').textContent = currentSentence.ko;
+    sentenceEl.querySelector('h1').textContent = currentSentence.en; 
 };
 
 getSentences();
